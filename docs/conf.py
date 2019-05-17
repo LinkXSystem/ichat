ded4daf07a69485b2742e3cff5c48ff2c293d90b
yangzhi <yz@lejurobot.com>
2019-05-17 19:29:23 +0800

    :wrench: missing leaked information, sorry

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+
+
+# SMTP
+
+SMTP_HOST = 'smtp.163.com'
+SMTP_PORT = 25
+SMTP_USERNAME = ''
+SMTP_PASSWORD = ''
+
+# SSH
+
+SSH_HOST = '0.0.0.0'
+SSH_PORT = 22
+SSH_USERNAME = ''
+SSH_PASSWORD = ''

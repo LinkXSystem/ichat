4d609ef0700eb16ebdf17e185b57a8588a44e297
zhi <linkasde_system@163.com>
2019-10-21 00:11:02 +0800

    :sparkles: 更新 ichat 的定义

--- ichat/__init__.py
+++ ichat/__init__.py
@@ -2,5 +2,8 @@
 
 from .loader import (Downloader, ScpServer, SCPClient)
 from .mail import (Postman)
+from .robot import (Robot)
+from .utils import (CommonUtils, IsUtils)
+from .github import (Repository)
 
-__all__ = ['Postman', 'Downloader', 'ScpServer', 'SCPClient']
+__all__ = ['Postman', 'Downloader', 'ScpServer', 'SCPClient', 'Robot', 'CommonUtils', 'IsUtils', 'Repository']
